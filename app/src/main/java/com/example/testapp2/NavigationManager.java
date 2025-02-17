513322f2be49597069c2fe01693d9f30f9066106
BoberCoder <samatkhasanov6@vk.com>
2025-02-18 01:21:33 +0500

    0.4.8(navigation bar update)

--- app/src/main/java/com/example/testapp2/NavigationManager.java
+++ app/src/main/java/com/example/testapp2/NavigationManager.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import com.example.testapp2.Activity.Account.AccountActivity;
 import com.example.testapp2.Activity.Account.History;
 import com.example.testapp2.Activity.Learn;
+import com.example.testapp2.Activity.MainActivity;
 import com.example.testapp2.Activity.Search;
 import com.example.testapp2.Activity.Settings;
 
@@ -13,16 +14,16 @@ public class NavigationManager {
     public static void onNavigationSelected(Context context, int id) {
         Intent intent = null;
 
-        if (id == R.id.nav_search) {
-            intent = new Intent(context, Search.class);
-        } else if (id == R.id.nav_account) {
-            intent = new Intent(context, AccountActivity.class);
-        } else if (id == R.id.nav_history) {
-            intent = new Intent(context, History.class);
+        if (id == R.id.nav_home) {
+            intent = new Intent(context, MainActivity.class);
         } else if (id == R.id.nav_learn) {
             intent = new Intent(context, Learn.class);
+        } else if (id == R.id.nav_search) {
+            intent = new Intent(context, Search.class);
         } else if (id == R.id.nav_settings) {
             intent = new Intent(context, Settings.class);
+        } else if (id == R.id.nav_info) {
+            intent = new Intent(context, InfoActivity.class);
         }
 
         if (intent != null) {
