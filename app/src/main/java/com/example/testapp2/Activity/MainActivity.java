095a18935dd2dca4e5259d2c0cab448da73b0757
Adelaaas <adela.x@yandex.ru>
2025-02-19 15:35:37 +0300

    Main_activity update

--- app/src/main/java/com/example/testapp2/Activity/MainActivity.java
+++ app/src/main/java/com/example/testapp2/Activity/MainActivity.java
@@ -2,29 +2,25 @@ package com.example.testapp2.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.testapp2.Activity.Account.AccountActivity;
 import com.example.testapp2.Data.database.DatabaseHelper;
-import com.example.testapp2.NavigationManager;
 import com.example.testapp2.R;
 import com.example.testapp2.app.dataBaseApp;
 import com.example.testapp2.databinding.ActivityMainBinding;
-import com.example.testapp2.fragments.LoginFragment;
-import com.google.android.material.navigation.NavigationView;
+import com.example.testapp2.databinding.ActivitySearchBinding;
+import com.example.testapp2.databinding.ActivitySettingsBinding;
 import com.example.testapp2.fragments.LoginFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends BaseActivity {
-
-    private ActivityMainBinding binding;
-    private DrawerLayout drawer;
+public class MainActivity extends AppCompatActivity {
 
     // Экземпляр DatabaseHelper
     private DatabaseHelper databaseHelper;
@@ -32,8 +28,14 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot()); // Правильный setContentView
+//        setContentView(R.layout.activity_main); // Правильный setContentView
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Устанавливаем верхний тулбар
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+
         databaseHelper = dataBaseApp.getDatabaseHelper();
 
         findViewById(R.id.start_search).setOnClickListener(v -> {
@@ -45,23 +47,65 @@ public class MainActivity extends BaseActivity {
             Intent intent = new Intent(this, Learn.class);
             startActivity(intent);
         });
-        // Устанавливаем верхний тулбар
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        // Скрыть кнопку "Назад"
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setHomeButtonEnabled(false);
-        }
+//        // Скрыть кнопку "Назад"
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//            getSupportActionBar().setHomeButtonEnabled(false);
+//        }
 
-        // Настраиваем нижний тулбар
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+        // Переход по кнопкам в нижнем тулбаре
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                int id = item.getItemId();
 
+                if (id == R.id.nav_learn) {
+                    startActivity(new Intent(getApplicationContext(), Learn.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                else if (id == R.id.nav_search) {
+                    startActivity(new Intent(getApplicationContext(), Search.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                else if (id == R.id.nav_settings) {
+                    startActivity(new Intent(getApplicationContext(), Settings.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                else if (id == R.id.nav_info) {
+                    startActivity(new Intent(getApplicationContext(), InfoActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                else if (id == R.id.nav_home) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
-
+    // Для перехода по иконке аккаунта
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.up_menu_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.nav_account) {
+            startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+            overridePendingTransition(0, 0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private String getUserEmail() {
         // Логика для получения email пользователя
         LoginFragment loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.email);
@@ -69,30 +113,6 @@ public class MainActivity extends BaseActivity {
             return loginFragment.getEmail();
         }
         return null; // или возвращайте пустую строку, если email не найден
-    }
-
-
-
-    /*private void startAccountActivity() {
-        Intent intent = new Intent(this, AccountActivity.class);
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            // Log the exception to investigate
-            android.util.Log.e("MainActivity", "Error starting AccountActivity", e);
-            // Toast notification to show error
-            android.widget.Toast.makeText(this, "AccountActivity not found", android.widget.Toast.LENGTH_SHORT).show();
-
-        }
-    }*/
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
-
-    protected int getSelectedMenuItemId() {
-        return R.id.nav_home;
     }
 
 }
